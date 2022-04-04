22ca16eec058aecd641c8c77b614d43849242ed6
Kaito.K <cassava1126@gmail.com>
2022-04-04 17:31:58 +0900

    feat: ReduxStoreデータの保持 Persistorの設定途中

--- src/components/redux/store.ts
+++ src/components/redux/store.ts
@@ -1,6 +1,17 @@
 import { createStore } from 'redux'
 import { userReducer as user } from './User'
+import { persistReducer, persistStore } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 
-const store = createStore(user)
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['config'],
+}
+
+const persistedReducer = persistReducer(persistConfig, user)
+
+const store = createStore(persistedReducer)
+export const persistor = persistStore(store)
 
 export default store
