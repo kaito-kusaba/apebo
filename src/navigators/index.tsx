0c9d85c3ce0791be27f0ae027b643e289faa285e
Kaito Kusaba <40602595+AokhaSak@users.noreply.github.com>
2022-04-29 18:02:33 +0900

    feat: import global component

--- src/navigators/index.tsx
+++ src/navigators/index.tsx
@@ -1,15 +1,14 @@
-import Header from 'components/organisms/Header'
 import { BrowserRouter as Router } from 'react-router-dom'
 import MainNavigator from './Main'
+import NavigationBar from 'components/organisms/NavigationBar'
 
 export default function RootNavigator() {
   return (
     <div>
       <Router>
-        <Header />
+        <NavigationBar />
         <MainNavigator />
       </Router>
     </div>
-
   )
 }
