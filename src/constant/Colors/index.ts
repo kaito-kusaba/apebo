3ee4e80973cd1e68bf981e0dfed7066a8ab10289
Kaito Kusaba <40602595+AokhaSak@users.noreply.github.com>
2022-04-29 17:53:31 +0900

    feat: constant colors

---
+++ src/constant/Colors/index.ts
@@ -0,0 +1,11 @@
+// Black to White
+export const BLACK = '#000000'
+export const BLACK_WASH = '#0C0C0C'
+export const CHAOS_BLACK = '#0F0F0F'
+export const EERIE_BLACK = '#1A1A1A'
+export const JET = '#353535'
+export const BLACK_TIE = '#474747'
+export const SQUANT = '#666666'
+export const GREY = '#B6B6B6'
+export const PORPOISE = '#DADADA'
+export const WHITE = '#FFFFFF'
