9edbe249a4960a3ff2033bf171dccea159ee5758
Kaito Kusaba <40602595+AokhaSak@users.noreply.github.com>
2022-04-29 14:17:19 +0900

    Revert "fix: update post icon"
    This reverts commit a53402879ea0953d37a40947605527619c661294.

--- src/pages/Top/index.tsx
+++ src/pages/Top/index.tsx
@@ -1,12 +1,10 @@
 import UserIcon from 'components/atoms/UserIcon'
-import NavigationButton from 'components/organisms/Button/Navigation'
 import React from 'react'
 
 export default React.memo(function TopScreen() {
   return (
     <div>
-      <UserIcon size={46} href={'/'} />
-      <NavigationButton type="Home" />
+      <UserIcon size={40} href={"/"} />
     </div>
   )
-})
+}, )
--- src/types/NavigationButtonTypes.ts
+++
@@ -1,1 +0,0 @@
-export type NavigationButtonTypes = "Home" | "Account" | "Settings" | "Post"
