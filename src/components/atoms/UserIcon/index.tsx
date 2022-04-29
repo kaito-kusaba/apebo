26dc2687c6ab81307effd4719c51060230aa84db
Kaito Kusaba <40602595+AokhaSak@users.noreply.github.com>
2022-04-29 17:59:56 +0900

    feat: link to user account url

--- src/components/atoms/UserIcon/index.tsx
+++ src/components/atoms/UserIcon/index.tsx
@@ -4,11 +4,15 @@ import type { UserIconSize } from 'types/UserIconSize'
 import { useInjection } from './hooks'
 
 interface Props {
-    size: UserIconSize
-    href: string
+  size: UserIconSize
 }
 
-export default React.memo(function UserIcon({ size, href }: Props) {
-    const { src } = useInjection({ size })
-    return <a href={href} className='icon-container'><img src={src} width={size} height={size} /></a>
+export default React.memo(function UserIcon({ size }: Props) {
+  const { src, user } = useInjection({ size })
+  const url = `account/${user.uid}`
+  return (
+    <a href={url} className="icon-container">
+      <img src={src} width={size} height={size} />
+    </a>
+  )
 })
