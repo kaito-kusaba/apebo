007b1b4a489f616e17556ffe282a82eae6bdd2c6
Kaito Kusaba <40602595+AokhaSak@users.noreply.github.com>
2022-04-29 18:02:52 +0900

    feat: update topScreen page

--- src/pages/Top/index.tsx
+++ src/pages/Top/index.tsx
@@ -1,12 +1,5 @@
-import UserIcon from 'components/atoms/UserIcon'
-import NavigationButton from 'components/organisms/Button/Navigation'
 import React from 'react'
 
 export default React.memo(function TopScreen() {
-  return (
-    <div>
-      <UserIcon size={46} href={'/'} />
-      <NavigationButton type="Home" />
-    </div>
-  )
+  return <div></div>
 })
