049015076f5f8266589b9a5ebcd77675f805c636
Kaito Kusaba <40602595+AokhaSak@users.noreply.github.com>
2022-05-04 00:14:48 +0900

    feat: update AccountScreen Component

--- src/pages/Account/index.tsx
+++ src/pages/Account/index.tsx
@@ -1,5 +1,22 @@
-import React from 'react'
+import SafeView from 'components/atoms/SafeView'
+import ScreenLabel from 'components/molecules/Label/ScreenLabel'
+import { RootState } from 'components/redux'
+import React, { useEffect, useState } from 'react'
+import { useSelector } from 'react-redux'
 
-export default function AccountScreen() {
-  return <div>account page</div>
-}
+export default React.memo(function AccoutScreen() {
+  const { username } = useSelector(({ user }: RootState) => user)
+  const [label, setLabel] = useState<string>(username)
+
+  useEffect(() => {
+    if (!username) {
+      setLabel('匿名さん')
+    }
+  }, [username])
+
+  return (
+    <SafeView>
+      <ScreenLabel label={label} />
+    </SafeView>
+  )
+})
