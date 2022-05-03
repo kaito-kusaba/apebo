f23fca4c408ad05980a973ac0e2bcf2659a26024
Kaito Kusaba <40602595+AokhaSak@users.noreply.github.com>
2022-05-04 00:03:20 +0900

    feat: update userIcon component

--- src/components/atoms/UserIcon/index.tsx
+++ src/components/atoms/UserIcon/index.tsx
@@ -1,17 +1,19 @@
 import React from 'react'
-import './style.css'
 import type { UserIconSize } from 'types/UserIconSize'
 import { useInjection } from './hooks'
+import { useStyles } from './style'
 
 interface Props {
   size: UserIconSize
+  style?: string
 }
 
-export default React.memo(function UserIcon({ size }: Props) {
+export default React.memo(function UserIcon({ size, style }: Props) {
   const { src, user } = useInjection({ size })
+  const styles = useStyles()
   const url = `account/${user.uid}`
   return (
-    <a href={url} className="icon-container">
+    <a href={url} className={`${styles.iconContainer} ${style}`}>
       <img src={src} width={size} height={size} />
     </a>
   )
