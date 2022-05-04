2792198236d22e226272aa7c2e34d8a1b384df98
Kaito Kusaba <40602595+AokhaSak@users.noreply.github.com>
2022-05-05 05:09:30 +0900

    feat: update account screen

--- src/pages/Account/index.tsx
+++ src/pages/Account/index.tsx
@@ -1,12 +1,16 @@
 import SafeView from 'components/atoms/SafeView'
 import ScreenLabel from 'components/molecules/Label/ScreenLabel'
+import PostContent from 'components/organisms/ListItems/PostContent'
+import ProfileContainer from 'components/organisms/ProfileContainer'
 import { RootState } from 'components/redux'
 import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
+import { useStyles } from './style'
 
 export default React.memo(function AccoutScreen() {
   const { username } = useSelector(({ user }: RootState) => user)
   const [label, setLabel] = useState<string>(username)
+  const styles = useStyles()
 
   useEffect(() => {
     if (!username) {
@@ -15,8 +19,10 @@ export default React.memo(function AccoutScreen() {
   }, [username])
 
   return (
-    <SafeView>
+    <SafeView style={styles.containerStyle}>
       <ScreenLabel label={label} />
+      <ProfileContainer />
+      <PostContent />
     </SafeView>
   )
 })
---
+++ src/pages/Account/style.ts
@@ -0,0 +1,10 @@
+import { css } from '@emotion/css'
+
+export function useStyles() {
+  const containerStyle = css`
+    padding: 0 8px;
+  `
+  return {
+    containerStyle,
+  }
+}
