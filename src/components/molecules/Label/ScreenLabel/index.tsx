21e75af2495374e274b16e165c4a41c608ae0729
Kaito Kusaba <40602595+AokhaSak@users.noreply.github.com>
2022-05-05 05:07:06 +0900

    feat: update settings label component

--- src/components/molecules/Label/ScreenLabel/index.tsx
+++ src/components/molecules/Label/ScreenLabel/index.tsx
@@ -1,4 +1,6 @@
+import { SettingsIcon } from 'components/atoms/Icon'
 import React from 'react'
+import { Link, useLocation } from 'react-router-dom'
 import { useStyles } from './style'
 
 interface Props {
@@ -7,5 +9,23 @@ interface Props {
 
 export default React.memo(function ScreenLabel({ label }: Props) {
   const styles = useStyles()
-  return <div className={styles.screenLabel}>{label}</div>
+  const location = useLocation()
+  if (location.pathname === '/account') {
+    return (
+      <div className={styles.screenLabelContainer}>
+        <div>
+          <h1 className={styles.screenLabel}>{label}</h1>
+          <span className={styles.screenLabelSubTitle}>件の投稿</span>
+        </div>
+        <Link to="/settings">
+          <img src={SettingsIcon} className={styles.settingsImg} />
+        </Link>
+      </div>
+    )
+  }
+  return (
+    <div className={styles.screenLabelContainer}>
+      <h1 className={styles.screenLabel}>{label}</h1>
+    </div>
+  )
 })
--- src/components/molecules/Label/ScreenLabel/style.ts
+++ src/components/molecules/Label/ScreenLabel/style.ts
@@ -2,15 +2,32 @@ import { css } from '@emotion/css'
 import { Colors } from 'constant'
 
 export function useStyles() {
-  const screenLabel = css`
-    padding: 24px 0 24px 24px;
+  const screenLabelContainer = css`
+    padding: 14px 24px;
     background-color: ${Colors.BLACK_WASH};
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  `
+  const screenLabel = css`
     color: ${Colors.WHITE};
     font-size: 19px;
     font-weight: bold;
   `
+  const screenLabelSubTitle = css`
+    color: ${Colors.SQUANT};
+    font-size: 12px;
+  `
+
+  const settingsImg = css`
+    width: 24px;
+    height: 24px;
+  `
 
   return {
+    screenLabelContainer,
     screenLabel,
+    screenLabelSubTitle,
+    settingsImg,
   }
 }
