d0f3646fd211e9baca25980731749c7e279240e6
Kaito Kusaba <40602595+AokhaSak@users.noreply.github.com>
2022-05-06 02:01:44 +0900

    feat: disabledProps分岐

--- src/components/atoms/UserIcon/index.tsx
+++ src/components/atoms/UserIcon/index.tsx
@@ -7,16 +7,25 @@ import { useStyles } from './style'
 interface Props {
   size: UserIconSize
   style?: string
+  disabled?: boolean
 }
 // TODO: すでに/account/${user.uid}の時は画像を拡大表示する
 
-export default React.memo(function UserIcon({ size, style }: Props) {
+export default React.memo(function UserIcon({ size, disabled, style }: Props) {
   const { src, user } = useInjection({ size })
   const styles = useStyles()
   const url = `/account/${user.uid}`
-  return (
-    <Link to={url} className={`${styles.iconContainer} ${style}`}>
-      <img src={src} width={size} height={size} />
-    </Link>
-  )
+  if (disabled) {
+    return (
+      <div className={`${styles.iconContainer} ${style}`}>
+        <img src={src} width={size} height={size} />
+      </div>
+    )
+  } else {
+    return (
+      <Link to={url} className={`${styles.iconContainer} ${style}`}>
+        <img src={src} width={size} height={size} />
+      </Link>
+    )
+  }
 })
