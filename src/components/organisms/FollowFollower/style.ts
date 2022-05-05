b5bf70f1ca532c5708b1eb4fa2c29f417b125b69
Kaito Kusaba <40602595+AokhaSak@users.noreply.github.com>
2022-05-05 19:38:11 +0900

    feat: update follow follower container css hover

--- src/components/organisms/FollowFollower/style.ts
+++ src/components/organisms/FollowFollower/style.ts
@@ -2,13 +2,20 @@ import { css } from '@emotion/css'
 import { Colors } from 'constant'
 
 export function useStyles() {
-  const followFollowerContainer = css``
+  const followFollowerContainer = css`
+    padding-bottom: 8px;
+  `
   const followFollowerLink = css`
     text-decoration: none;
     color: ${Colors.WHITE};
     font-weight: bold;
     font-size: 14px;
     margin-right: 6px;
+    transition: all 0.2s;
+    &:hover {
+      opacity: 0.6;
+      text-decoration: underline;
+    }
   `
   const followFollowerText = css`
     color: ${Colors.SQUANT};
