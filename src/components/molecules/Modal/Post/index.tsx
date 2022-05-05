6a1235ed32ec72a2fa886e78ca6adbe8b5a32903
Kaito Kusaba <40602595+AokhaSak@users.noreply.github.com>
2022-05-06 02:01:09 +0900

    feat: 高さが自動可変する投稿入力モーダルの作成

--- src/components/molecules/Modal/Post/index.tsx
+++ src/components/molecules/Modal/Post/index.tsx
@@ -1,27 +1,70 @@
+import UserIcon from 'components/atoms/UserIcon'
+import UserName from 'components/atoms/UserName'
 import { RootState } from 'components/redux'
 import { actions } from 'components/redux/Modal'
-import React, { useCallback } from 'react'
+import React, { useCallback, useEffect, useRef, useState } from 'react'
 import Modal from 'react-modal'
 import { useDispatch, useSelector } from 'react-redux'
-
-const customStyles = {
-  content: {
-    top: '50%',
-    left: '50%',
-    right: 'auto',
-    bottom: 'auto',
-    marginRight: '-50%',
-    transform: 'translate(-50%, -50%)',
-  },
-}
+import { useStyles } from './style'
 
 export default React.memo(function PostModal() {
+  const styles = useStyles()
   const dispatch = useDispatch()
   const { isOpen } = useSelector(({ modal }: RootState) => modal)
+  const [text, setText] = useState<string>('')
+  const textAreaRef = useResizeTextArea(text)
 
   const onClose = useCallback(() => {
     dispatch(actions.setModal(false))
   }, [])
 
-  return <Modal isOpen={isOpen} style={customStyles} onRequestClose={onClose}></Modal>
+  const onChangeText = useCallback(
+    e => {
+      setText(e.target.value)
+    },
+    [text],
+  )
+
+  function useResizeTextArea(value: string | undefined) {
+    const ref = useRef<HTMLTextAreaElement>(null)
+    useEffect(() => {
+      const element = ref.current
+      if (!element) {
+        return
+      }
+      const { borderTopWidth, borderBottomWidth, paddingTop, paddingBottom } = getComputedStyle(element)
+
+      element.style.height = 'auto'
+      element.style.height = `calc(${element.scrollHeight}px + ${paddingTop} + ${paddingBottom} + ${borderTopWidth} + ${borderBottomWidth})`
+    }, [value])
+
+    return ref
+  }
+
+  return (
+    <Modal
+      isOpen={isOpen}
+      appElement={document.getElementById('root') as HTMLElement}
+      className={styles.modalContainer}
+      overlayClassName={styles.overlay}
+      onRequestClose={onClose}>
+      <div className={styles.header}>
+        <UserIcon disabled size={46} style={styles.userIcon} />
+        <UserName hideUid />
+      </div>
+      <textarea
+        onChange={onChangeText}
+        maxLength={200}
+        placeholder="今から一緒にゲームしない？"
+        className={styles.textArea}
+        ref={textAreaRef}
+      />
+      <div className={styles.textAreaInfoContainer}>
+        <img src="" alt="nikochan" />
+        <div>
+          <span className={styles.textAreaLength}>{text.length}/200</span>
+        </div>
+      </div>
+    </Modal>
+  )
 })
---
+++ src/components/molecules/Modal/Post/style.ts
@@ -0,0 +1,64 @@
+import { css } from '@emotion/css'
+import { Colors } from 'constant'
+
+export function useStyles() {
+  const modalContainer = css`
+    background-color: ${Colors.BLACK_WASH};
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    right: auto;
+    bottom: auto;
+    margin-right: -50%;
+    transform: translate(-50%, -50%);
+    width: 524px;
+    border-radius: 12px;
+  `
+  const overlay = css`
+    position: fixed;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    background-color: rgba(0, 0, 0, 0.4);
+  `
+
+  const header = css`
+    display: flex;
+    margin: 24px auto 16px 24px;
+  `
+
+  const userIcon = css`
+    margin-right: 8px;
+  `
+  const textArea = css`
+    resize: none;
+    background-color: ${Colors.BLACK_WASH};
+    border: none;
+    padding: 0 24px;
+    width: 100%;
+    outline: none;
+    color: ${Colors.PORPOISE};
+    font-size: 19px;
+    overflow: hidden;
+  `
+
+  const textAreaInfoContainer = css`
+    display: flex;
+  `
+  const textAreaLength = css`
+    color: ${Colors.GREY};
+    font-size: 16px;
+    font-weight: 100;
+  `
+
+  return {
+    modalContainer,
+    overlay,
+    header,
+    userIcon,
+    textArea,
+    textAreaInfoContainer,
+    textAreaLength,
+  }
+}
