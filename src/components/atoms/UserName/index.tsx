03a4cdb2c081f8cf709a26578743eb359d7c08ce
Kaito Kusaba <40602595+AokhaSak@users.noreply.github.com>
2022-05-06 02:01:27 +0900

    feat: uid条件分岐

--- src/components/atoms/UserName/index.tsx
+++ src/components/atoms/UserName/index.tsx
@@ -5,15 +5,16 @@ import { useStyles } from './style'
 
 interface Props {
   style?: string
+  hideUid?: boolean
 }
 
-export default React.memo(function UserName({ style }: Props) {
+export default React.memo(function UserName({ style, hideUid }: Props) {
   const styles = useStyles()
   const { user } = useSelector(({ user }: RootState) => user)
   return (
     <div className={`${styles.userNameContainerStyle} ${style}`}>
       <div className={styles.userName}>[0000] Ringosi{user.displayName}</div>
-      <div className={styles.userId}>@userid532423</div>
+      {!hideUid && <div className={styles.userId}>@userid532423</div>}
     </div>
   )
 })
