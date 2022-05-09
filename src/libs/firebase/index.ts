1166e290064699a91429a197e1d0dacb2e13d414
Kaito Kusaba <40602595+AokhaSak@users.noreply.github.com>
2022-05-09 23:15:54 +0900

    feat: refactor firebase

--- src/libs/firebase/index.ts
+++ src/libs/firebase/index.ts
@@ -5,21 +5,20 @@ import { actions } from 'components/redux/User'
 import store from 'components/redux/store'
 
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 }
 
 initializeApp(firebaseConfig)
 
 export const auth = getAuth()
 
-
-onAuthStateChanged(auth, (user) => {
+onAuthStateChanged(auth, user => {
   const dispatch = store.dispatch
   if (user) {
     // User is signed in
@@ -29,4 +28,3 @@ onAuthStateChanged(auth, (user) => {
     dispatch(actions.clearUser())
   }
 })
-
