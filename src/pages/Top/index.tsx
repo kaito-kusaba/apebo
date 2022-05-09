8339822a98a4a022e0f6bac1741334c06f2345a9
Kaito Kusaba <40602595+AokhaSak@users.noreply.github.com>
2022-05-09 21:49:08 +0900

    feat: import TimeLineTabList

--- src/pages/Top/index.tsx
+++ src/pages/Top/index.tsx
@@ -1,6 +1,7 @@
 import SafeView from 'components/atoms/SafeView'
 import ScreenLabel from 'components/molecules/Label/ScreenLabel'
 import PostContent from 'components/organisms/ListItems/PostContent'
+import TimeLineTabList from 'components/organisms/ListItems/TimeLineTabList'
 import React from 'react'
 import { useStyles } from './style'
 
@@ -9,6 +10,7 @@ export default React.memo(function TopScreen() {
   return (
     <SafeView style={styles.postContainer}>
       <ScreenLabel label="ホーム" />
+      <TimeLineTabList />
       <PostContent />
       <PostContent />
       <PostContent />
