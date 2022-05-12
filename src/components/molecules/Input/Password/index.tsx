1bef4e5d574b7a971b4465d1f78363b6f18619fe
Kaito Kusaba <40602595+AokhaSak@users.noreply.github.com>
2022-05-13 00:20:26 +0900

    feat: create PasswordInput Component

---
+++ src/components/molecules/Input/Password/index.tsx
@@ -0,0 +1,33 @@
+import React, { useCallback, useState } from 'react'
+import { useStyles } from './style'
+import { EyeHideIconSquant, EyeIconSquant } from 'components/atoms/Icon'
+
+interface Props {
+  value: string
+  onChange: (e: any) => void
+}
+
+export default React.memo(function PasswordInput({ value, onChange, ...props }: Props) {
+  const [hidden, setHidden] = useState<boolean>(false)
+  const styles = useStyles()
+
+  const onClick = useCallback(() => {
+    setHidden(!hidden)
+  }, [hidden])
+
+  return (
+    <div className={styles.container}>
+      <input
+        className={styles.passwordInput}
+        value={value}
+        onChange={onChange}
+        type={hidden ? 'password' : 'text'}
+        placeholder="パスワード"
+        {...props}
+      />
+      <button onClick={onClick} className={styles.button}>
+        <img src={hidden ? EyeHideIconSquant : EyeIconSquant} alt="" className={styles.passwordInputImg} />
+      </button>
+    </div>
+  )
+})
---
+++ src/components/molecules/Input/Password/style.ts
@@ -0,0 +1,48 @@
+import { css } from '@emotion/css'
+import { Colors } from 'constant'
+
+export function useStyles() {
+  const container = css`
+    position: relative;
+    width: 100%;
+  `
+  const passwordInput = css`
+    padding: 10px 0 6px 0;
+    color: ${Colors.WHITE};
+    font-size: 16px;
+    outline: none;
+    border: none;
+    background-color: transparent;
+    transition: all ease 0.3s;
+    width: 100%;
+    margin-bottom: 10px;
+    border-bottom: 1px ${Colors.CHAOS_BLACK} solid;
+    &:focus {
+      border-bottom: 1px ${Colors.WHITE} solid;
+    }
+  `
+  const passwordInputImg = css`
+    width: 20px;
+    height: 20px;
+  `
+
+  const button = css`
+    position: absolute;
+    top: 10px;
+    right: 0;
+    width: 20px;
+    height: 20px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: transparent;
+    border: none;
+    outline: none;
+  `
+  return {
+    container,
+    passwordInput,
+    passwordInputImg,
+    button,
+  }
+}
