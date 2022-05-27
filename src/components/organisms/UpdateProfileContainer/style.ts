dc7732903580ad57ca2b08469262dfa02ab6cb47
unkly <u7gyuuu7gi@gmail.com>
2022-05-27 10:38:44 +0900

    feat:プロフィール画面の変更

---
+++ src/components/organisms/UpdateProfileContainer/style.ts
@@ -0,0 +1,28 @@
+import { css } from '@emotion/css'
+import { Colors } from 'constant'
+
+export function useStyles() {
+  const container = css`
+    display: flex;
+    flex-direction: column;
+    color: ${Colors.WHITE};
+    background-color: ${Colors.CHAOS_BLACK};
+    padding: 0px 24px;
+    height: 100vh;
+  `
+
+  const top = css`
+    font-size: 19px;
+    padding: 24px 0;
+    width: 862px;
+    font-weight: 700;
+  `
+
+  const label = css``
+
+  return {
+    container,
+    top,
+    label,
+  }
+}
