c29342628ba79db4726c340d19d893f44f9f7d28
Kaito Kusaba <40602595+AokhaSak@users.noreply.github.com>
2022-05-29 00:59:20 +0900

    feat: アカウントの投稿件数を取得

--- src/components/molecules/Label/ScreenLabel/index.tsx
+++ src/components/molecules/Label/ScreenLabel/index.tsx
@@ -1,4 +1,6 @@
+import { RootState } from 'components/redux'
 import React from 'react'
+import { useSelector } from 'react-redux'
 import { useLocation } from 'react-router-dom'
 import { useStyles } from './style'
 
@@ -9,12 +11,14 @@ interface Props {
 export default React.memo(function ScreenLabel({ label }: Props) {
   const styles = useStyles()
   const location = useLocation()
+  const { posts } = useSelector(({ accountPosts }: RootState) => accountPosts)
+
   if (location.pathname === '/account') {
     return (
       <div className={styles.screenLabelContainer}>
         <div>
           <h1 className={styles.screenLabel}>{label}</h1>
-          <span className={styles.screenLabelSubTitle}>件の投稿</span>
+          <span className={styles.screenLabelSubTitle}>{posts.length}件の投稿</span>
         </div>
       </div>
     )
