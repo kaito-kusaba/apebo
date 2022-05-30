b765cc8e9d17d1145290c3d63c069ee6fb4f3e7f
unkly <u7gyuuu7gi@gmail.com>
2022-05-30 23:32:58 +0900

    feat:アカウント画面の情報をデータと紐づける

--- src/components/organisms/AccountURL/index.tsx
+++ src/components/organisms/AccountURL/index.tsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import { LinkIconGray } from 'components/atoms/Icon'
 import { useStyles } from './style'
+import { useSelector } from 'react-redux'
+import { RootState } from 'components/redux'
 
 export default React.memo(function AccountURL() {
-  const url = 'https://google.com/google.com/google.com'
   const styles = useStyles()
+  const { userData } = useSelector(({ user }: RootState) => user)
+
   return (
     <div className={styles.container}>
       <img className={styles.img} src={LinkIconGray} alt="" />
-      <a href={url} target="_blank" rel="noopener" className={styles.url}>
-        {url}
+      <a href={userData.website} target="_blank" rel="noopener" className={styles.url}>
+        {userData.website}
       </a>
     </div>
   )
