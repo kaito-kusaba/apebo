0bbd7d19831a1a39db56f26b49d43b91e418df30
Kaito Kusaba <40602595+AokhaSak@users.noreply.github.com>
2022-06-01 21:23:56 +0900

    fix: ユーザーデータの内容を個別にもってくる

--- src/components/organisms/DiscordID/index.tsx
+++ src/components/organisms/DiscordID/index.tsx
@@ -1,19 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useStyles } from './style'
 import { CopyToClipboard } from 'react-copy-to-clipboard'
 import { DiscordIconGray } from 'components/atoms/Icon'
 import { useSelector } from 'react-redux'
 import { RootState } from 'components/redux'
+import { useLocation, useParams } from 'react-router-dom'
+import { doc, DocumentData, getDoc } from 'firebase/firestore'
+import { db } from 'libs/firebase'
 
 export default React.memo(function DiscordID() {
   const styles = useStyles()
   const { userData } = useSelector(({ user }: RootState) => user)
+  const params = useParams()
+  const location = useLocation()
+  const [data, setData] = useState<DocumentData>()
+
+  const fetchUserData = async () => {
+    if (params.uid) {
+      const userRef = doc(db, 'users', params.uid)
+      const userSnap = await getDoc(userRef)
+      setData(userSnap.data())
+    }
+  }
+
+  useEffect(() => {
+    fetchUserData()
+  }, [])
+
   return (
     <div className={styles.discordIdCopy}>
       <CopyToClipboard text={userData.discordId!} onCopy={() => alert('IDをコピーしました')}>
         <div className={styles.discordIdContainer}>
           <img src={DiscordIconGray} alt="" className={styles.discordIcon} />
-          <div className={styles.discordId}>{userData.discordId}</div>
+          <div className={styles.discordId}>
+            {location.pathname === '/account' ? userData.discordId : data?.discord_id}
+          </div>
         </div>
       </CopyToClipboard>
     </div>
