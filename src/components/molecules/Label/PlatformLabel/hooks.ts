e37de0f6f37207baf36a59496e5c06d8925b92ae
Kaito Kusaba <40602595+AokhaSak@users.noreply.github.com>
2022-06-11 21:48:59 +0900

    feat: プラットフォームの表示

--- src/components/molecules/Label/PlatformLabel/hooks.ts
+++ src/components/molecules/Label/PlatformLabel/hooks.ts
@@ -1,5 +1,4 @@
-import { MobileIcon, PCIcon, PSIcon, SwitchIcon, XboxIcon } from 'components/atoms/Icon'
-import { doc, DocumentData, DocumentReference, getDoc } from 'firebase/firestore'
+import { doc, getDoc } from 'firebase/firestore'
 import { db } from 'libs/firebase'
 import { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
@@ -7,50 +6,22 @@ import { useParams } from 'react-router-dom'
 import { RootState } from 'components/redux'
 
 export function useInjection() {
-  const imgsTemp: string[] = []
-  const [imgs, setImgs] = useState<string[]>([])
   const { user } = useSelector(({ user }: RootState) => user)
   const params = useParams<{ uid: string }>()
-  const [ref, setRef] = useState<DocumentReference<DocumentData>>(doc(db, 'users', user!.uid))
-  const [platforms, setPlatforms] = useState<number[]>()
+  const [platforms, setPlatforms] = useState<number[]>([])
 
   const fetchPlatforms = async () => {
+    const uid = params.uid ? params.uid : user!.uid
+    const ref = doc(db, 'users', uid)
     const mySnap = await getDoc(ref)
-    setPlatforms(mySnap.data()?.platforms)
+    await setPlatforms(mySnap.data()?.platforms || [])
   }
 
   useEffect(() => {
-    if (params) {
-      setRef(doc(db, 'users', params.uid!))
-    }
     fetchPlatforms()
-  }, [])
-
-  useEffect(() => {
-    platforms?.map(platform => {
-      switch (platform) {
-        case 0:
-          imgsTemp.push(PCIcon)
-          break
-        case 1:
-          imgsTemp.push(PSIcon)
-          break
-        case 2:
-          imgsTemp.push(SwitchIcon)
-          break
-        case 3:
-          imgsTemp.push(XboxIcon)
-          break
-        case 4:
-          imgsTemp.push(MobileIcon)
-          break
-      }
-      setImgs(imgsTemp)
-    })
   }, [])
 
   return {
     platforms,
-    imgs,
   }
 }
