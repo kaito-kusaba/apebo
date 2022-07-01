9bbfaa3ed4a5b42e30e7f3f5498e80969a9950bc
unkly <u7gyuuu7gi@gmail.com>
2022-07-01 12:47:04 +0900

    feat:followlistとfollowerlistを別containerに分け＋コメントで言われた場所のコードの修正

--- src/components/organisms/Containers/FollowFollowersListContainer/hooks.ts
+++ src/components/organisms/Containers/FollowFollowersListContainer/hooks.ts
@@ -1,55 +1,24 @@
-import { collection, onSnapshot, query, where } from 'firebase/firestore'
-import { db } from 'libs/firebase'
 import { useCallback, useEffect, useState } from 'react'
 import { useLocation, useNavigate, useParams } from 'react-router-dom'
 
 export function useInjection() {
   const [label, setLabel] = useState<string>('')
-  const followTemp: string[] = []
-  const followerTemp: string[] = []
-  const [followerList, setFollowerList] = useState<string[]>([])
-  const [followList, setFollowList] = useState<string[]>([])
+  const [pageIndex, setPageIndex] = useState<number>()
   const navigate = useNavigate()
   const params = useParams()
   const location = useLocation()
 
-  const fetchFollowData = async () => {
-    const ref = collection(db, 'follows')
-    const followsCollectionRef = query(ref, where('follow_id', '==', params.uid))
-    onSnapshot(followsCollectionRef, querySnapshot => {
-      querySnapshot.forEach(follow => {
-        //INFO:フォロー欄に変更が入るともともとfollowTempに入ってる人もpushされていたのでif文で条件分岐
-        if (followTemp.find(item => item === follow.data()?.follower_id)) {
-        } else {
-          followTemp.push(follow.data()?.follower_id)
-        }
-      })
-      setFollowList(followTemp)
-    })
-  }
-
-  const fetchFollowerData = async () => {
-    const ref = collection(db, 'follows')
-    const followsCollectionRef = query(ref, where('follower_id', '==', params.uid))
-    onSnapshot(followsCollectionRef, querySnapshot => {
-      querySnapshot.forEach(follower => {
-        //INFO:上記と同様 TODO:(リロードしないとフォロー中からフォロー外しても残ったままになってします)
-        if (followerTemp.find(item => item === follower.data()?.follow_id)) {
-        } else {
-          followerTemp.push(follower.data()?.follow_id)
-        }
-      })
-      setFollowerList(followerTemp)
-    })
-  }
-
   useEffect(() => {
-    fetchFollowData()
-    fetchFollowerData()
-  }, [])
-
-  useEffect(() => {
-    setLabel(location.pathname.match(/follows/) ? 'フォロー中' : 'フォロワー')
+    switch (location.pathname) {
+      case `/account/${params.uid}/follows`:
+        setLabel('フォロー中')
+        setPageIndex(0)
+        break
+      case `/account/${params.uid}/followers`:
+        setLabel('フォロワー')
+        setPageIndex(1)
+        break
+    }
   }, [location])
 
   const onClickFollow = useCallback(() => {
@@ -64,7 +33,8 @@ export function useInjection() {
     onClickFollower,
     onClickFollow,
     label,
-    followList,
-    followerList,
+    location,
+    params,
+    pageIndex,
   }
 }
--- src/components/organisms/Containers/FollowFollowersListContainer/index.tsx
+++ src/components/organisms/Containers/FollowFollowersListContainer/index.tsx
@@ -1,39 +1,37 @@
 import BackButtonLabel from 'components/molecules/Label/BackButtonLabel'
-import FollowFollowerContent from 'components/organisms/ListItems/FollowFollowerContent'
 import React from 'react'
-import { useLocation } from 'react-router-dom'
+import FollowerListContainer from '../FollowerListContainer'
+import FollowListContainer from '../FollowListContainer'
 import { useInjection } from './hooks'
 import { useStyles } from './style'
 
 export default function FollowFollowersListContainer() {
   const styles = useStyles()
-  const { onClickFollow, onClickFollower, label, followList, followerList } = useInjection()
-  const location = useLocation()
+  const { onClickFollow, onClickFollower, label, pageIndex } = useInjection()
+
+  const ContentsList: React.VFC = () => {
+    switch (pageIndex) {
+      case 0:
+        return <FollowListContainer />
+      case 1:
+        return <FollowerListContainer />
+      default:
+        return <></>
+    }
+  }
 
   return (
     <div className={styles.container}>
       <BackButtonLabel label={label} />
       <div className={styles.selectButton}>
-        <span className={styles.followerButton()} onClick={onClickFollower}>
+        <div className={styles.followerButton()} onClick={onClickFollower}>
           <span className={styles.followerLabel()}>フォロワー</span>
-        </span>
-        <span className={styles.followButton()} onClick={onClickFollow}>
+        </div>
+        <div className={styles.followButton()} onClick={onClickFollow}>
           <span className={styles.followLabel()}>フォロー中</span>
-        </span>
+        </div>
       </div>
-      {location.pathname.match(/follows/) ? (
-        <div>
-          {followList.map(follow => {
-            return <FollowFollowerContent uid={follow} key={follow} />
-          })}
-        </div>
-      ) : (
-        <div>
-          {followerList.map(follower => {
-            return <FollowFollowerContent uid={follower} key={follower} />
-          })}
-        </div>
-      )}
+      <ContentsList />
     </div>
   )
 }
