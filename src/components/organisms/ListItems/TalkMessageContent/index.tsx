497d9b9e8241cd6b48152a27d7c260e53ac94410
unkly <u7gyuuu7gi@gmail.com>
2022-07-04 22:49:26 +0900

    feat:メッセージに三点リーダーを追加

--- src/components/organisms/ListItems/TalkMessageContent/index.tsx
+++ src/components/organisms/ListItems/TalkMessageContent/index.tsx
@@ -1,8 +1,9 @@
 import Time from 'components/atoms/Time'
 import UserIcon from 'components/atoms/UserIcon'
 import UserName from 'components/atoms/UserName'
+import ActionButton from 'components/molecules/Button/Action'
 import { Timestamp } from 'firebase/firestore'
-import React from 'react'
+import React, { useCallback, useState } from 'react'
 import { useStyles } from './style'
 
 type Props = {
@@ -12,19 +13,26 @@ type Props = {
 }
 
 export default function TalkMessageContent({ message, uid, time }: Props) {
-  const styles = useStyles()
+  const [isHovered, setIsHovered] = useState<boolean>(false)
+  const styles = useStyles({ isHovered })
+
+  const onMouseToggle = useCallback(() => {
+    setIsHovered(!isHovered)
+  }, [isHovered, setIsHovered])
+
   return (
-    <div className={styles.container}>
-      <div className={styles.leftContainer}>
-        <UserIcon uid={uid} size={46} />
-        <div>
-          <div className={styles.rightContainer}>
-            <UserName uid={uid} noDisplayUid style={styles.userName} />
-            <Time time={time} />
-          </div>
-          <pre className={styles.content}>{message}</pre>
+    <div className={styles.container} onMouseEnter={onMouseToggle} onMouseLeave={onMouseToggle}>
+      <div className={styles.contentHeader}>
+        <div className={styles.contentUser}>
+          <UserIcon uid={uid} size={46} />
+          <UserName uid={uid} noDisplayUid style={styles.userName} />
+          <Time time={time} />
+        </div>
+        <div className={styles.actionButton()}>
+          <ActionButton type="Other" uid={uid} />
         </div>
       </div>
+      <pre className={styles.content}>{message}</pre>
     </div>
   )
 }
--- src/components/organisms/ListItems/TalkMessageContent/style.ts
+++ src/components/organisms/ListItems/TalkMessageContent/style.ts
@@ -1,18 +1,32 @@
 import { css } from '@emotion/css'
+import { Colors } from 'constant'
 
-export function useStyles() {
+type Props = {
+  isHovered: boolean
+}
+
+export function useStyles({ isHovered }: Props) {
   const container = css`
+    width: 1202px;
+    display: flex;
+    flex-direction: column;
     padding: 16px 24px;
+    transition: 0.2s;
+    :hover {
+      background-color: ${Colors.EERIE_BLACK};
+    }
   `
 
-  const leftContainer = css`
+  const contentHeader = css`
+    display: flex;
+    width: 100%;
+    align-items: flex-start;
+    justify-content: space-between;
+  `
+
+  const contentUser = css`
     display: flex;
     align-items: flex-start;
-  `
-
-  const rightContainer = css`
-    display: flex;
-    align-items: center;
   `
 
   const userName = css`
@@ -21,14 +35,27 @@ export function useStyles() {
 
   const content = css`
     margin-top: 8px;
-    margin-left: 16px;
+    margin-left: 62px;
   `
+
+  const actionButton = () => {
+    if (isHovered) {
+      return css`
+        opacity: 100%;
+      `
+    } else {
+      return css`
+        opacity: 0%;
+      `
+    }
+  }
 
   return {
     container,
-    leftContainer,
-    rightContainer,
+    contentHeader,
+    contentUser,
     userName,
     content,
+    actionButton,
   }
 }
